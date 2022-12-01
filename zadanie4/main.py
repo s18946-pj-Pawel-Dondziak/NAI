026e2224ccdb2d2e7de7dbc0c34040b09465c9ad
s18946-pj-Pawel-Dondziak <s18946@pjwstk.edu.pl>
2022-12-01 21:05:00 +0100

    final commit of zadanie4

--- zadanie4/main.py
+++ zadanie4/main.py
@@ -1,33 +1,63 @@
-import numpy as np
-import matplotlib.pyplot as plt
-from sklearn import svm, datasets
+import pandas as pd
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import train_test_split
+from sklearn import metrics
+from sklearn.svm import SVC
+'''
+Jakub Świderski s19443
+Paweł Dondziak s18946
 
-# import some data to play with
-abalones = datasets.load_iris("abalone.data") # nie umim załadować tego pliku
-print(abalones)
-X = abalones.data[:, :2] # we only take the first two features. We could
- # avoid this ugly slicing by using a two-dim dataset
-y = abalones.target
+To properly run the program you need to install sklearn and pandas
 
-# we create an instance of SVM and fit out data. We do not scale our
-# data since we want to plot the support vectors
-svc = svm.SVC(kernel='rbf', C=1, gamma=100).fit(X, y)
+This program is used to predict disease based on Decision Tree Classifier data and SVC.
+'''
 
-# create a mesh to plot in
-x_min, x_max = X[:, 0].min() - 1, X[:, 0].max() + 1
-y_min, y_max = X[:, 1].min() - 1, X[:, 1].max() + 1
-h = (x_max / x_min)/100
-xx, yy = np.meshgrid(np.arange(x_min, x_max, h),
- np.arange(y_min, y_max, h))
+def read_data(col_names, feature_cols, target, csv_file):
+    '''
+    This function id used to load the data from  .csv data
 
-plt.subplot(1, 1, 1)
-Z = svc.predict(np.c_[xx.ravel(), yy.ravel()])
-Z = Z.reshape(xx.shape)
-plt.contourf(xx, yy, Z, cmap=plt.cm.Paired, alpha=0.8)
+    Parameters:
+    col_names: csv file header
+    feature_cols: features
+    target: target to learn
+    csv_file: path to csv file
 
-plt.scatter(X[:, 0], X[:, 1], c=y, cmap=plt.cm.Paired)
-plt.xlabel('Sepal length')
-plt.ylabel('Sepal width')
-plt.xlim(xx.min(), xx.max())
-plt.title('SVC with linear kernel')
-plt.show()
+    Returns: List containing train-test split of inputs.
+    ''' 
+    pima = pd.read_csv(csv_file, header=None, names=col_names)
+    X = pima[feature_cols]
+    y = pima[target]
+    return train_test_split(X, y, test_size=0.3)
+
+def calculate(clf, X_train, X_test, y_train, y_test):
+    '''
+    This function is used to calculate the score.
+
+    Params:
+    clf:
+    X_train: The training input samples
+    X_test: The input samples
+    y_train: The target values
+    y_test:  Ground truth (correct) labels
+
+    Returns: The fraction of correctly classified samples
+    '''
+    clf = clf.fit(X_train, y_train)
+    y_pred = clf.predict(X_test)
+    return metrics.accuracy_score(y_test, y_pred)
+
+print("Pima Indians Diabetes")
+col_names = ['pregnant', 'glucose', 'bp', 'skin', 'insulin', 'bmi', 'pedigree', 'age', 'label']
+feature_cols = ['pregnant', 'insulin', 'bmi', 'age','glucose','bp','pedigree']
+X_train, X_test, y_train, y_test = read_data(col_names, feature_cols, "label", "pima-indians-diabetes.csv")
+
+print("Decision Tree Classifer Accuracy:", calculate(DecisionTreeClassifier(), X_train, X_test, y_train, y_test))
+print("SVC Accuracy:", calculate(SVC(), X_train, X_test, y_train, y_test))
+
+print("Kyphosis")
+col_names = ["Kyphosis","Age","Number","Start"]
+feature_cols = ["Age","Number","Start"]
+X_train, X_test, y_train, y_test = read_data(col_names, feature_cols, "Kyphosis", "kyphosis.csv")
+
+print("Decision Tree Classifer Accuracy:", calculate(DecisionTreeClassifier(), X_train, X_test, y_train, y_test))
+print("SVC Accuracy:", calculate(SVC(), X_train, X_test, y_train, y_test))
